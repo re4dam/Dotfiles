9aeeabc9594e4c1d595e49c1e935adb1a305e4e2
re4dam <zakidam332@gmail.com>
2025-07-21 17:21:34 +0700

    modified install script and added powerlevel10k prompt

--- setup.sh
+++ setup.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# update latest package
-sudo pacman -Syu
+# Prompt the user confirmation to perform system wide update
+read -p "Do you want to perform system update? (y/n) " confirm
 
-# download stow package through pacman
-sudo pacman -S stow
+# Check user's input was 'y' or 'Y'
+if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
+    echo "Performing system wide update"
+    sudo pacman -Syu
+else
+    echo "Update skipped"
+fi
 
-# create symbolic links using stow to all config/local directory
+# download desired arch packages for personal used
+packages=(stow kitty neovim fastfetch neovide tmux ssh ttf-jetbrains-mono-nerd inter-font exa npm)
+sudo pacman -S "$packages"
 stow .
-
-# download desired packages for personal used
-packages=(kitty neovim fastfetch neovide tmux ssh ttf-jetbrains-mono-nerd inter-font exa)
 
 # configuring terminal GUI
 # download oh-my-zsh
@@ -25,11 +29,35 @@ git clone https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-${ZSH:-
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# arch user repository
+# configuring tmux and plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# arch user repository packages
 mkdir ~/git/
-# zen-browser-bin
-# redhat-fonts
-# sddm-silent-theme
-git clone https://aur.archlinux.org/zen-browser-bin.git ~/git/
-git clone https://aur.archlinux.org/redhat-fonts.git ~/git/
-git clone https://aur.archlinux.org/sddm-silent-theme.git ~/git/
+cd ~/git/
+
+# list of aur-packages to install
+aur-packages=("zen-browser-bin" "redhat-fonts" "sddm-silent-theme")
+
+for package in "${aur-packages[@]}"; do
+    echo "Installing $package"
+
+    # Clone repository from AUR
+    git clone "https://aur.archlinux.org/$package.git"
+
+    # change directory to package
+    cd $package
+
+    # Build and install package
+    # -s: sync and install dependencies.
+    # -i: installs the package.
+    makepkg -si
+
+    # return to git directory
+    cd ~/git/
+
+    echo "$package installed success"
+done
+
+# return to home directory
+cd
