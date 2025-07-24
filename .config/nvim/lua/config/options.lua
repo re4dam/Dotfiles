6789fd0779243d33bf5685bc4f5198d03e20b978
re4dam <zakidam332@gmail.com>
2025-07-24 10:35:48 +0700

    improved neovim unique identifier

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -33,3 +33,22 @@ if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
   vim.g.neovide_theme = "auto"
 end
+
+-- Enable setting the window title
+vim.o.title = true
+
+-- Define the format for the window title
+-- %{getpid()} gets the unique process ID
+-- %t shows just the filename
+-- %m shows a [+] if the file is modified
+vim.o.titlestring = "Neovide [PID:%{getpid()}] - %t %m"
+
+-- Creates a command that takes one argument (the name)
+vim.api.nvim_create_user_command("NameWindow", function(opts)
+  -- Sets the title to your argument, plus the filename and modified status
+  vim.o.titlestring = opts.args .. " - %t %m"
+end, { nargs = 1, desc = "Set a custom name for the Neovide window title" })
+
+vim.api.nvim_create_user_command("ResetTitle", function()
+  vim.o.titlestring = "Neovide [PID:%{getpid()}] - %t %m"
+end, { nargs = 0, desc = "Reset the Neovide window title to the default" })
