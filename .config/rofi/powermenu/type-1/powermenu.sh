47c570a39bcc90f2b23a45a48a5099b11911b392
re4dam <zakidam332@gmail.com>
2026-03-03 08:27:59 +0700

    switch powermenu to use rofi

--- .config/rofi/powermenu/type-1/powermenu.sh
+++ .config/rofi/powermenu/type-1/powermenu.sh
@@ -99,6 +99,8 @@ $lock)
         betterlockscreen -l
     elif [[ -x '/usr/bin/i3lock' ]]; then
         i3lock
+    elif [[ -x '/usr/bin/hyprlock' ]]; then
+        hyprlock
     fi
     ;;
 $suspend)
